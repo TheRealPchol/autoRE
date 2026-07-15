39e6b1c036006782c66a67f015a06b2ff95afcfa
TheRealPchol <therealpchol7@gmail.com>
2026-07-15 21:16:45 +0300

    Update code

--- runph.py
+++ runph.py
@@ -1,8 +1,6 @@
 import asyncio
 from src.ph import main
 import src.ph as ph
-ph.onefile = True
-asyncio.run(main.shell_loop())
 def runph():
     ph.onefile = True
     asyncio.run(main.shell_loop())
